6b636fdc38d1a4362c2eeca2813d426ccf380bb9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-22 18:01:38 -0500

    feat(profile): += ::on(), ::off() (#10)

--- init.zsh
+++ init.zsh
@@ -35,11 +35,58 @@ p6df::modules::cloudsmith::external::brews() {
 #  Returns:
 #	str - str
 #
+#  Environment:	 CLOUDSMITH_API_KEY P6_DFZ_PROFILE_CLOUDSMITH
 #>
 ######################################################################
 p6df::modules::cloudsmith::prompt::mod() {
 
-  local str="cloudsmith:        pgollucci"
+  local str
+  if ! p6_string_blank "$P6_DFZ_PROFILE_CLOUDSMITH"; then
+    str="cloudsmith:\t  $P6_DFZ_PROFILE_CLOUDSMITH:"
+    if ! p6_string_blank "$CLOUDSMITH_API_KEY"; then
+      str=$(p6_string_append "$str" "api" " ")
+    fi
+  fi
 
   p6_return_str "$str"
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::cloudsmith::profile::on(profile, [api_key=])
+#
+#  Args:
+#	profile -
+#	OPTIONAL api_key - []
+#
+#  Environment:	 CLOUDSMITH_API_KEY P6_DFZ_PROFILE_CLOUDSMITH
+#>
+######################################################################
+p6df::modules::cloudsmith::profile::on() {
+  local profile="$1"
+  local api_key="${2:-}"
+
+  p6_env_export "P6_DFZ_PROFILE_CLOUDSMITH" "$profile"
+  if ! p6_string_blank "$api_key"; then
+    p6_env_export "CLOUDSMITH_API_KEY" "$api_key"
+  fi
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::cloudsmith::profile::off()
+#
+#  Environment:	 CLOUDSMITH_API_KEY P6_DFZ_PROFILE_CLOUDSMITH
+#>
+######################################################################
+p6df::modules::cloudsmith::profile::off() {
+
+  p6_env_export_un P6_DFZ_PROFILE_CLOUDSMITH
+  p6_env_export_un CLOUDSMITH_API_KEY
+
+  p6_return_void
 }

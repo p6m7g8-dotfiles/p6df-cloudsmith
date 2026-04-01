9d0492caf1d6ceb63f85fd28bc3b30d15d0cc7a1
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 15:01:28 -0400

    fix(prompt): split p6_return_words word and variable into separate args (#18)
    * fix(prompt): split p6_return_words word and variable into separate args
    
    Replace prompt::mod(), profile::on(), and profile::off() with a single
    profile::mod() returning cloudsmith and CLOUDSMITH_API_KEY.
    
    * fix(prompt): use double quotes for variable expansion in p6_return_words (SC2016)

--- init.zsh
+++ init.zsh
@@ -30,72 +30,15 @@ p6df::modules::cloudsmith::external::brews() {
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::cloudsmith::prompt::mod()
+# Function: words cloudsmith $CLOUDSMITH_API_KEY = p6df::modules::cloudsmith::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - cloudsmith $CLOUDSMITH_API_KEY
 #
-#  Environment:	 BUNDLE_DL__CLOUDSMITH__IO CLOUDSMITH_API_KEY NPM_AUTH_TOKEN P6_DFZ_PROFILE_CLOUDSMITH
+#  Environment:	 CLOUDSMITH_API_KEY
 #>
 ######################################################################
-p6df::modules::cloudsmith::prompt::mod() {
+p6df::modules::cloudsmith::profile::mod() {
 
-  local str
-  if p6_string_blank_NOT "$P6_DFZ_PROFILE_CLOUDSMITH"; then
-    str="cloudsmith:\t  $P6_DFZ_PROFILE_CLOUDSMITH:"
-    if p6_string_blank_NOT "$CLOUDSMITH_API_KEY"; then
-      str=$(p6_string_append "$str" "api" " ")
-    fi
-    if p6_string_blank_NOT "$NPM_AUTH_TOKEN"; then
-      str=$(p6_string_append "$str" "cs:npm" " ")
-    fi
-    if p6_string_blank_NOT "$BUNDLE_DL__CLOUDSMITH__IO"; then
-      str=$(p6_string_append "$str" "cs:rg" " ")
-    fi
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::cloudsmith::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block (export CLOUDSMITH_API_KEY=... CLOUDSMITH_ENTITLEMENT_TOKEN=...)
-#
-#  Environment:	 BUNDLE_DL__CLOUDSMITH__IO CLOUDSMITH_API_KEY CLOUDSMITH_ENTITLEMENT_TOKEN NPM_AUTH_TOKEN P6_DFZ_PROFILE_CLOUDSMITH
-#>
-######################################################################
-p6df::modules::cloudsmith::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_CLOUDSMITH" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::cloudsmith::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 BUNDLE_DL__CLOUDSMITH__IO CLOUDSMITH_API_KEY CLOUDSMITH_ENTITLEMENT_TOKEN NPM_AUTH_TOKEN P6_DFZ_PROFILE_CLOUDSMITH
-#>
-######################################################################
-p6df::modules::cloudsmith::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_CLOUDSMITH
-
-  p6_return_void
+  p6_return_words 'cloudsmith' "$"
 }
